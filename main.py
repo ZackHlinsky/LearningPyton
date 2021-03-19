a09a5f7c6143643054ec9b0efde8d001a029bd95
ZackHlinsky <819460@mystma.org>
2021-03-19 15:27:21 +0000

    mad lib

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Cars\nStar wars the empire strikes back\nHome alone\nBack to the future\nCentral intelligence\n")
+favFood = "pasta"
+day = "today"
+print("Your favorite food is " + favFood + " " + day)
+adj1 = dark
+adj2 = old
+bird1 = duck
+room1 = living room 
+verb1 = walked 
+verb2 = cook 
+name1 = dad
+noun1 = apple
+liquid1 = milk
+verb3 = amazing 
+print("It was a " + adj1 + ", cold November day. I woke up to the " + adj2 + "")
