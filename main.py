773e203e5649e5566b56f0961a2d2103789ff84f
ZackHlinsky <819460@mystma.org>
2021-03-22 14:56:18 +0000

    MadLib

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
-favFood = "pasta"
-day = "today"
-print("Your favorite food is " + favFood + " " + day)
-adj1 = dark
-adj2 = old
-bird1 = duck
-room1 = living room 
-verb1 = walked 
-verb2 = cook 
-name1 = dad
-noun1 = apple
-liquid1 = milk
-verb3 = amazing 
-print("It was a " + adj1 + ", cold November day. I woke up to the " + adj2 + "")
+
+adj1 = "dark"
+adj2 = "old"
+bird1 = "duck"
+room1 = "living room" 
+verb1 = "walked"
+verb2 = "cook" 
+name1 = "dad"
+noun1 = "apple"
+liquid1 = "milk"
+verb3 = "amazing" 
+print("It was a " + adj1 + ", cold November day. I woke up to the " + adj2 + " smell of " + bird1 + " roasting in the " + room1 + " downstairs. I " + verb1 + " down the stairs to see if I could help " + verb2 + " the dinner. My mom said, see if " + name1 + " needs a fresh " + noun1 + " So I carried a tray of glasses full of " + liquid1 + " into the " + verb3 + " room.")
