dc932bc62fbb4882b6ebf15494aac662212cf931
Hlinsky Zachary <819460@stma.k12.mn.us>
2021-03-26 10:23:14 -0500

    Learning if statements

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
+light = input("Stoplight Color:\n")
 
-adj1 = input("What is an adjective?\n")
-adj2 = input("What is an adjective?\n")
-bird1 = input("What is a type of bird?\n")
-room1 = input("What is a room in a house\n")
-verb1 = input("What is a verb in the past tense?\n")
-verb2 =input("What is a verb?\n")
-name1 = input("What is a relatives name?\n")
-noun1 = input("What is a noun?\n")
-liquid1 = input("What is a liquid?\n")
-verb3 = input("What is a verb ending in -ing?\n")
-print("It was a " + adj1 + ", cold November day. I woke up to the " + adj2 + " smell of " + bird1 + " roasting in the " + room1 + " downstairs. I " + verb1 + " down the stairs to see if I could help " + verb2 + " the dinner. My mom said, see if " + name1 + " needs a fresh " + noun1 + " So I carried a tray of glasses full of " + liquid1 + " into the " + verb3 + " room.")
+if light == "green":
+    print("Go")
+
+elif light == "yellow":
+    print("Slow")
+
+elif light == "blue":
+    print("CALL 911")
+
+else:
+    print("Stop")
